96339b0eda730bd0b8be76771046ca1e09c91cc4
Borislav Stanimirov <b@ibob.bg>
2025-09-01 07:28:05 +0300

    fix 2d result, start 3d test

--- finufft-vs-ducc/finufft-vs-ducc.cpp
+++ finufft-vs-ducc/finufft-vs-ducc.cpp
@@ -58,7 +58,7 @@ void test_1d(
             K_SIZE,
             fxs.data(),
             fks.data(),
-            1, // iflag
+            1, // inverse fourier
             EPS,
             U_SIZE,
             r_finufft.data(),
@@ -68,7 +68,7 @@ void test_1d(
 
     std::vector<std::complex<double>> r_ducc(U_SIZE);
     {
-        ducc0::cmav<double, 2> dxs(xs.data(), { size_t(K_SIZE), 1 });
+        ducc0::cmav<double, 2> dxs(xs.data(), {size_t(K_SIZE), 1 });
 
         ducc0::Nufft<double, double, double> nufft(
             false,
@@ -83,7 +83,7 @@ void test_1d(
         );
 
         nufft.nu2u(
-            false, // forward
+            false, // forward = false (inverse fourier)
             0, // verbosity
             ducc0::cmav<std::complex<double>, 1>{ks.data(), {ks.size()}},
             ducc0::vfmav<std::complex<double>>{r_ducc.data(), {r_ducc.size()}}
@@ -125,7 +125,7 @@ void test_2d(
             K_SIZE,
             fxs.data(), fys.data(),
             fks.data(),
-            1,  // iflag
+            1, // inverse fourier
             EPS,
             U_SIZE_X, U_SIZE_Y,
             r_finufft.data(),
@@ -133,17 +133,19 @@ void test_2d(
         );
     }
 
+    // NOTE:
+    // x and y are swapped in ducc, both in shape and in coords
     std::vector<std::complex<double>> r_ducc(r_finufft.size());
     {
         std::vector<double> dcoords(K_SIZE * 2);
         for (int i = 0; i < K_SIZE; ++i) {
-            dcoords[i * 2 + 0] = xs[i];
-            dcoords[i * 2 + 1] = ys[i];
+            dcoords[i * 2 + 1] = xs[i];
+            dcoords[i * 2 + 0] = ys[i];
         }
 
         ducc0::cmav<double, 2> vdcoords(dcoords.data(), {size_t(K_SIZE), 2});
 
-        std::vector<size_t> ushape = {size_t(U_SIZE_X), size_t(U_SIZE_Y)};
+        std::vector<size_t> ushape = {size_t(U_SIZE_Y), size_t(U_SIZE_X)};
 
         ducc0::Nufft<double, double, double> nufft(
             false,
@@ -158,13 +160,14 @@ void test_2d(
         );
 
         nufft.nu2u(
-            false, // forward
+            false, // forward = false (inverse fourier)
             0, // verbosity
             ducc0::cmav<std::complex<double>, 1>{ks.data(), {ks.size()}},
             ducc0::vfmav<std::complex<double>>{r_ducc.data(), ushape}
         );
     }
 
+    std::cout << "finufft                                          |  ducc\n";
     for (int y = 0; y < U_SIZE_Y; ++y) {
         std::string finufft_row, ducc_row;
         for (int x = 0; x < U_SIZE_X; ++x) {
@@ -180,6 +183,99 @@ void test_2d(
         }
         std::cout << finufft_row << " |  " << ducc_row;
         std::cout << "\n";
+    }
+}
+
+void test_3d(
+    const std::vector<double>& xs,
+    const std::vector<double>& ys,
+    const std::vector<double>& zs,
+    const std::vector<std::complex<double>>& ks,
+    const int U_SIZE_X,
+    const int U_SIZE_Y,
+    const int U_SIZE_Z
+) {
+    std::cout << "3D\n";
+
+    const int K_SIZE = int(xs.size());
+
+    std::vector<std::complex<double>> r_finufft(U_SIZE_X * U_SIZE_Y * U_SIZE_Z);
+    {
+        auto fxs = xs;
+        auto fys = ys;
+        auto fzs = zs;
+        auto fks = ks;
+
+        finufft_opts opts;
+        finufft_default_opts(&opts);
+        opts.nthreads = 1;
+
+        finufft3d1(
+            K_SIZE,
+            fxs.data(), fys.data(), fzs.data(),
+            fks.data(),
+            1, // inverse fourier
+            EPS,
+            U_SIZE_X, U_SIZE_Y, U_SIZE_Z,
+            r_finufft.data(),
+            &opts
+        );
+    }
+
+    std::vector<std::complex<double>> r_ducc(r_finufft.size());
+    {
+        std::vector<double> dcoords(K_SIZE * 3);
+        for (int i = 0; i < K_SIZE; ++i) {
+            dcoords[i * 2 + 2] = xs[i];
+            dcoords[i * 2 + 1] = ys[i];
+            dcoords[i * 2 + 0] = zs[i];
+        }
+
+        ducc0::cmav<double, 2> vdcoords(dcoords.data(), {size_t(K_SIZE), 2});
+
+        std::vector<size_t> ushape = {size_t(U_SIZE_Z), size_t(U_SIZE_Y), size_t(U_SIZE_X)};
+
+        ducc0::Nufft<double, double, double> nufft(
+            false,
+            vdcoords,
+            ushape,
+            EPS,
+            1, // nthreads
+            1.5, 2.5, // sigma min-max
+            {2 * pi, 2 * pi, 2 * pi}, // periodicty
+            false, // fft_order
+            {0, 0, 0} //origin
+        );
+
+        nufft.nu2u(
+            false, // forward = false (inverse fourier)
+            0, // verbosity
+            ducc0::cmav<std::complex<double>, 1>{ks.data(), {ks.size()}},
+            ducc0::vfmav<std::complex<double>>{r_ducc.data(), ushape}
+        );
+    }
+
+    std::cout << "finufft                                          |  ducc\n";
+    for (int z = 0; z < U_SIZE_Z; ++z) {
+        for (int y = 0; y < U_SIZE_Y; ++y) {
+            std::string finufft_row, ducc_row;
+            for (int x = 0; x < U_SIZE_X; ++x) {
+                const auto i = z * U_SIZE_Y * U_SIZE_X + y * U_SIZE_X + x;
+                finufft_row += std::format(
+                    "{:+4.4f}{:+4.4f}i ",
+                    r_finufft[i].real(), r_finufft[i].imag()
+                );
+                ducc_row += std::format(
+                    "{:+4.4f}{:+4.4f}i ",
+                    r_ducc[i].real(), r_ducc[i].imag()
+                );
+            }
+            std::cout << finufft_row << " |  " << ducc_row;
+            std::cout << "\n";
+        }
+        if (z != U_SIZE_Z - 1) {
+            std::cout << std::string(100, '-') << "\n";
+        }
     }
 }
 
@@ -207,6 +303,10 @@ int main() {
     const auto ys = make_coord_vec(K_SIZE, rng);
 
     test_2d(xs, ys, ks, 3, 5);
+
+    const auto zs = make_coord_vec(K_SIZE, rng);
+
+    test_3d(xs, ys, zs, ks, 3, 5, 2);
 
     return 0;
 }
